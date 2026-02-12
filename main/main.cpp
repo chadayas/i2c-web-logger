1d2eb04e7a0c01a972360c8a71cd8c163e2fbd73
chadayas <cgassoya@gmail.com>
2026-02-12 13:00:45 -0500

    bool calibration init

--- main/main.cpp
+++ main/main.cpp
@@ -139,21 +139,24 @@ WifiService::~WifiService(){
 	disconnect();
 }
 
+adc_oneshot_unit_handle_t adc1_handle;
+
+bool adc_calibration_init() 
+
 void init_adc1(){
-	adc_oneshot_unit_handle_t adc1_handle;
-    	adc_oneshot_unit_init_cfg_t init_config1 = {
+    	adc_oneshot_unit_init_cfg_t init_cfg = {
        		 .unit_id = ADC_UNIT_1,
     	};
-    	ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
+    	ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_cfg, &adc1_handle));
 
-    	adc_oneshot_chan_cfg_t config = {
-       		 .atten = EXAMPLE_ADC_ATTEN,
+    	adc_oneshot_chan_cfg_t cfg = {
+       		 .atten = ADC_ATTEN_DB_12,
         	.bitwidth = ADC_BITWIDTH_DEFAULT,
     	};
-    	ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_0, &config));
+    	ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_0, &cfg));
 
     	adc_cali_handle_t adc1_cali_chan0_handle = NULL;
-    	bool do_calibration1_chan0 = example_adc_calibration_init(ADC_UNIT_1, ADC_CHANNEL_0, ADC_ATTEN_DB_12, &adc1_cali_chan0_handle);
+    	bool do_calibration1_chan0 = adc_calibration_init(ADC_UNIT_1, ADC_CHANNEL_0, ADC_ATTEN_DB_12, &adc1_cali_chan0_handle);
 
 }
 
